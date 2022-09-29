5a455616cc61f23560588dc0d18e403718334120
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-09-29 19:26:04 -0300

    add middleware de CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -8,6 +8,30 @@ const app = express()
 
 // conexão com o banco de dados
 db.connect()
+
+// habilita cors para uma único endereço
+// app.use(cors({
+//     origin: 'http://127.0.0.1:5500'
+// }))
+
+// habilita cors para mais de um endereço
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://yourapp.com'];
+
+app.use(cors({
+  origin: function(origin, callback){
+
+    // Mobile Request
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) === -1){
+      let msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // habilita server para receber dados json
 app.use(express.json())
