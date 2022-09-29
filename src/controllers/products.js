6d64cfb4d0759c314caa521027bbdc2a04645699
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-09-29 13:30:36 -0300

    add metodo put

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -31,7 +31,27 @@ async function post(req, res){
     })
 }
 
+async function put(req, res){
+    const { id } = req.params
+
+    const product = await ProductsModel.findOneAndUpdate({_id: id}, req.body, { new:true })
+
+    res.send({
+        massage: 'success',
+        product
+    })
+    // const product = await ProductsModel.findOne({ _id: id })
+
+    // await product.updateOne(req.body)
+
+    // res.send({
+    //     message: 'success',
+    //     product,
+    // })
+}
+
 module.exports = {
     get,
     post,
+    put,
 }
