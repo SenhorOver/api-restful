6f7d27fcc2052583d598175dfce840c504d9e1f9
Marcos Vinicius Silva <MrcsVS@outlook.com>
2022-09-29 18:43:37 -0300

    add metodo delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -50,8 +50,21 @@ async function put(req, res){
     // })
 }
 
+async function remove(req, res){
+    const {id} = req.params
+
+    const remove = await ProductsModel.deleteOne({_id: id})
+
+    let message = remove.acknowledged ? 'success' : 'error'
+
+    res.send({
+        message,
+    })
+}
+
 module.exports = {
     get,
     post,
     put,
+    remove,
 }
